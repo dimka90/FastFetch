edd59dffe7dc98b90b291258dff691312a937c21
dimka90 <dimkayilrit@gmail.com>
2025-05-09 14:08:55 +0100

    modify the functions to be downloaded concurently

--- FastFetcher/src/main.rs
+++ FastFetcher/src/main.rs
@@ -9,20 +9,22 @@ fn command_line_input() -> Vec<String>{
 }
 
 
-async  fn save_file(content: String) -> Result<(), Error>{
-let _ = tokio::fs::write("dimka.txt", content.as_bytes()).await;
+async  fn save_file(file_name: String, content: String) -> Result<(), Error>{
+let _ = tokio::fs::write(file_name, content.as_bytes()).await;
 
 Ok(())
 }
-async fn download_file(url: String) -> Result<String, reqwest::Error>{
-
+async fn download_file(url: String) -> Result<Vec<String>, reqwest::Error>{
+    let filename = url.split('/').last().unwrap_or("dimka.txt").to_string();
     let get_file = reqwest::get(url)
                     .await?
                     .text()
                     .await?;
                 println!("{:?}", get_file);
-
-                Ok(get_file)
+let mut file_data = vec![];
+file_data.push(filename);
+file_data.push(get_file);
+Ok(file_data)
 }
 #[tokio::main]
 async  fn main() {
@@ -30,13 +32,23 @@ async  fn main() {
 
 let urls = command_line_input();
 
-for i in urls.into_iter(){
+let mut  handles = Vec::new();
 
-    let result = tokio::spawn(download_file(i)).await;
-    let content_to_save = result.unwrap().unwrap();
-    let save_result = tokio::spawn(save_file(content_to_save)).await;
+for url in urls{
 
+    let handle = tokio::spawn(async move {
+
+        let content = download_file(url).await.unwrap();
+
+        save_file(content[0].clone(), content[1].clone())
+    });
+
+handles.push(handle);
+}
+
+for handle in handles{
+    handle.await.unwrap().await.unwrap();
+}
 }
 
 
-}
